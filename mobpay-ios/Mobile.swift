5632de4f630773af3b86176cc4755d0a4113db5d
Allan Mageto <allanbmageto@gmail.com>
2019-06-03 14:53:27 +0300

    Fixed the  callbacks
    added mobile money

--- mobpay-ios/Mobile.swift
+++ mobpay-ios/Mobile.swift
@@ -9,25 +9,31 @@
 import Foundation
 
 public class Mobile {
+ 
     var phone:String;
-//    var  type:Type;
-    var provider:String;
-    var pattern:NSRegularExpression?;
-//    var providerType = ["MPESA","M-PESA","EAZZYPAY","Eazzy Pay"]
-
-    
+   
     public init (phone:String) {
-        self.provider = "meh"
         self.phone = phone;
-//        self.mobileFullyValid = false
-//    self.setType(type);
     }
-    
-//    var providerType
+ 
     
     func refreshValidity(){
         
     }
+    
+    
+    var provider:String{
+        get{
+            return providersAndPatterns(phone: phone).0
+        }
+    }
+    var pattern:NSRegularExpression{
+        get{
+            return providersAndPatterns(phone: phone).1
+        }
+    }
+    
+  
     var mobilePartiallyValid: Bool{
         get{
             return setMobilePartiallyValid();
@@ -43,7 +49,7 @@ public class Mobile {
     func setMobilePartiallyValid() -> Bool{
         let range = NSMakeRange(0, phone.count)
 //        provider = providersAndPatterns(phone: <#T##String#>)
-        return pattern!.firstMatch(in: phone, options: [], range: range) != nil;
+        return pattern.firstMatch(in: phone, options: [], range: range) != nil;
     }
     
     func setMobileFullyValid()-> Bool{
@@ -51,7 +57,9 @@ public class Mobile {
     }
     
 
-    func providersAndPatterns(phone: String)->Any{
+    func providersAndPatterns(phone: String)->(String,NSRegularExpression){
+        var provider:String;
+        var pattern:NSRegularExpression;
         let range = NSMakeRange(0, phone.count)
         let mpesaRegex = try! NSRegularExpression(pattern: "^(\\+?254|0)[7]([0-2][0-9]|[9][0-3])[0-9]{0,6}$")
         let eazzyPayRegex = try! NSRegularExpression(pattern: "^4[0-9]{1,12}(?:[0-9]{6})?$")
@@ -77,109 +85,6 @@ public class Mobile {
             return (provider,pattern);
         }
     }
+   
 }
 
-//public enum Type {
-//        MPESA("M-PESA"), EAZZYPAY("Eazzy Pay");
-//        public String value;
-//
-//        Type(String value) {
-//        this.value = value;
-//        }
-//
-//        @Override
-//        public String toString() {
-//        return value;
-//        }
-//    }
-
-//public class Mobile extends BaseObservable implements Serializable {
-//    private String phone;
-//    private Type type;
-//    private String provider;
-//    private boolean mobileFullyValid;
-//    private boolean mobilePartiallyValid;
-//    private Pattern pattern;
-//
-//    public Mobile(String phone, Type type) {
-//        this.phone = phone;
-//        this.setType(type);
-//    }
-//
-//    public Mobile() {
-//
-//    }
-//
-//    public void refreshValidity() {
-//        setMobilePartiallyValid(this.pattern != null && this.phone != null && this.pattern.matcher(this.phone).matches());
-//        setMobileFullyValid(this.mobilePartiallyValid && phone.length() == 10);
-//    }
-//
-//    public String getPhone() {
-//        return phone;
-//    }
-//
-//    public void setPhone(String phone) {
-//        this.phone = phone;
-//        refreshValidity();
-//    }
-//
-//    public Type getType() {
-//        return type;
-//    }
-//
-//    public void setType(Type type) {
-//        switch (type) {
-//        case MPESA:
-//            this.provider = "702";
-//            this.setPattern(Pattern.compile("^(\\+?254|0)[7]([0-2][0-9]|[9][0-3])[0-9]{0,6}$"));
-//            break;
-//        case EAZZYPAY:
-//            this.provider = "708";
-//            this.setPattern(Pattern.compile("^(?:254|\\+254|0)?76[34]([0-9]{0,6})$"));
-//            break;
-//        default:
-//            throw new IllegalArgumentException("The type selected does not have a corresponding provider set");
-//        }
-//        this.type = type;
-//    }
-//
-//    public String getProvider() {
-//        return provider;
-//    }
-//
-//    public void setProvider(String provider) {
-//        this.provider = provider;
-//    }
-//
-//    @Bindable
-//    public boolean isMobileFullyValid() {
-//        return mobileFullyValid;
-//    }
-//
-//    public void setMobileFullyValid(boolean mobileFullyValid) {
-//        this.mobileFullyValid = mobileFullyValid;
-//        notifyPropertyChanged(BR.mobileFullyValid);
-//    }
-//
-//    @Bindable
-//    public boolean isMobilePartiallyValid() {
-//        return mobilePartiallyValid;
-//    }
-//
-//    public void setMobilePartiallyValid(boolean mobilePartiallyValid) {
-//        this.mobilePartiallyValid = mobilePartiallyValid;
-//        notifyPropertyChanged(BR.mobilePartiallyValid);
-//    }
-//
-//    public Pattern getPattern() {
-//        return pattern;
-//    }
-//
-//    public void setPattern(Pattern pattern) {
-//        this.pattern = pattern;
-//        refreshValidity();
-//    }
-//
-//
-//}
